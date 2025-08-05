b3eadea7b1fe8a7a26fa214d7dddc16318a0efcd
RealEeveahy <mason.mclean@hotmail.com>
2025-08-05 14:37:37 +0800

    Source update 2

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -1,6 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Section({sectionName, children})
+{
+  return (
+    <div className="contentSection">
+      <h1 className="sectionHeader">
+        {sectionName}
+      </h1>
+      <div className="contentHolder">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+function Item({itemName, imgSource, children})
+{
+  return (
+    <div className="portfolioItem">
+      <img className="piImage" src={imgSource} alt={itemName + "-icon"}/>
+      <div className="descriptionContainer">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+function ItemLink({link, iconSource})
+{
+  return (
+    <a href={link}>
+      <img src={iconSource} className="itemLink"/>
+    </a>
+  )
+}
+
 function App() {
   return (
     <div>
@@ -34,69 +69,42 @@ function App() {
                     This is where a longer intro paragraph will go.
                 </p>
             </div>
-            <div id="games" className="contentSection">
-                <h1 className="sectionHeader">
-                    GAMES
-                </h1>
-                <div className="contentHolder">
-                    <div className="portfolioItem">
-                        <img className="piImage" src="resource/dd_icon.png" alt="DeliveryDriver-icon"/>
-                        <div className="descriptionContainer">
-                            <p className="itemDescription">
+            <Section sectionName={"GAMES"}>
+              <Item itemName={"Delivery Driver"} imgSource={"resource/dd_icon.png"}>
+                <p className="itemDescription">
                                 Delivery Driver is my first ever app store release, and a huge milestone for me in many ways. 
                                 This project gave me a lot of experience employing C# object-oriented practices alongside Unity 
                                 to create a sustainable gameplay loop with multiple objectives.
                                 <br/>For PC users, there is a (very) beta version available on itch.io.
-                            </p>
-                            <a href="https://apps.apple.com/app/id1671405752">
-                                <img src="resource/apple_icon.png" className="itemLink"/>
-                            </a>
-                            <a href="https://cleanestcat.itch.io/delivery-driver">
-                                <img src="resource/itch_icon.png" className="itemLink"/>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="portfolioItem">
-                        <div className="descriptionContainer">
-                            <p className="itemDescription">
+                </p>
+                <ItemLink link={"https://apps.apple.com/app/id1671405752"} iconSource={"resource/apple_icon.png"}/>
+                <ItemLink link={"https://cleanestcat.itch.io/delivery-driver"} iconSource={"resource/itch_icon.png"}/>
+              </Item>
+              <Item itemName={"Between2Stones"} imgSource={"resource/b2s_icon.png"}>
+                <p className="itemDescription">
                                 Between2Stones was my first time attempting a game-jam, being a submission to 
                                 GBJam 11 hosted on itch.io. This gave me the experience of developing a game that is 
                                 not only limited by time - 10 days - but also limited in graphical and conceptual scope, 
                                 where it was necessary that I incorporated the theme "Space" and only employed 4 colours. 
                                 The end result is a platformer puzzle game in which the player must clear each challenge with 
                                 limited time as the <i>space</i> slowly decreases.
-                            </p>
-                            <a href="https://cleanestcat.itch.io/between2stones">
-                                <img src="resource/itch_icon.png" className="itemLink"/>
-                            </a>
-                        </div>
-                        <img className="piImage" src="resource/b2s_icon.png" alt="8bitapoc-icon"/>
-                    </div>
-                    <div className="portfolioItem">
-                        <img className="piImage" src="resource/temp_apoc_icon.png" alt="8bitapoc-icon"/>
-                        <div className="descriptionContainer">
-                            <p className="itemDescription">
-                                8-Bit-Apocalypse was built as a proof-of-concept for a Game-Boy style game made within Unity, 
+                </p>
+                <ItemLink link={"https://cleanestcat.itch.io/between2stones"} iconSource={"resource/itch_icon.png"}/>
+              </Item>
+              <Item itemName={"8-Bit Apocalypse"} imgSource={"resource/temp_apoc_icon.png"}>
+                <p className="itemDescription">
+                                8-Bit-Apocalypse swas built as a proof-of-concept for a Game-Boy style game made within Unity, 
                                 which would eventually extend to my submission for GBJam 11. 
-                            </p>
-                            <a href="https://cleanestcat.itch.io/8bit-apocalypse">
-                                <img src="resource/itch_icon.png" className="itemLink"/>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="apps" className="contentSection">
-                <h1 className="sectionHeader">
-                    APPS
-                </h1>
-            </div>
-            <div id="misc" className="contentSection">
-                <h1 className="sectionHeader">
-                    OTHER PROJECTS
-                </h1>
-                
-            </div>
+                </p>
+                <ItemLink link={"https://cleanestcat.itch.io/delivery-driver"} iconSource={"resource/itch_icon.png"}/>
+              </Item>
+            </Section>
+            <Section sectionName={"APPS"}>
+
+            </Section>
+            <Section sectionName={"OTHER PROJECTS"}>
+
+            </Section>
         </div>
     </div>
   );
